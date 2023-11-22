8c527526b254153dfd7b9aad788c4a6a2292de5f
Duca Cosmin <cosminduka@yahoo.com>
2023-11-22 12:50:08 +0200

    Clock coupled with the directional light

--- Assets/Scripts/Day_Night_Cycle.cs
+++ Assets/Scripts/Day_Night_Cycle.cs
@@ -8,17 +8,47 @@ using UnityEngine;
 public class Day_Night_Cycle : MonoBehaviour
 {
     public Clock clock = new Clock();
+    public List<hour_position> light_position = new List<hour_position>();
+    public Light scene_light;
+    public Transform world;
+
     // Start is called before the first frame update
     void Start()
     {
-    
+        Vector3 new_rot = FindLightPosition();
+        scene_light.transform.rotation = Quaternion.Euler(new_rot);
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        //scene_light.transform.RotateAround(world.position, Vector3.right, 20 * Time.deltaTime);
+        scene_light.transform.Rotate(Vector3.right * Time.deltaTime * 20);
         clock.incrementSeconds(Time.deltaTime);
     }
+
+    Vector3 FindLightPosition()
+    {
+        Vector3 result = Vector3.zero;
+        for(int i = 0; i < light_position.Count; i++)
+        {
+            if (light_position[i].hour == clock.hours)
+            {
+                result = light_position[i].pos;
+                break;
+            }
+
+        }
+        return result;
+    }
+}
+
+[System.Serializable]
+public class hour_position
+{
+    public int hour;
+    public Vector3 pos;
 }
 
 [System.Serializable]
