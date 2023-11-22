f90d8d9782506c0ee19ba691f9b133300a4bf292
Duca Cosmin <cosminduka@yahoo.com>
2023-11-22 18:56:12 +0200

    Integration test and state change based on clock hour

--- Assets/Scripts/Day_Night_Cycle.cs
+++ Assets/Scripts/Day_Night_Cycle.cs
@@ -11,42 +11,69 @@ public class Day_Night_Cycle : MonoBehaviour
     public List<hour_position> light_position = new List<hour_position>();
     public Light scene_light;
 
+    public string current_day_state = "";
+
+    int current_hour = -1;
+
     // Start is called before the first frame update
     void Start()
     {
-        Vector3 new_rot = FindLightPosition();
-        scene_light.transform.rotation = Quaternion.Euler(new_rot);
+        hour_position new_rot = FindLightPosition();
+        scene_light.transform.rotation = Quaternion.Euler(new_rot.pos);
+        current_hour = new_rot.hour;
+        CheckForNewState();
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        //scene_light.transform.Rotate(Vector3.right * Time.deltaTime);
-
         float rotationX = (float) 360f / (float) (clock.hours_in_day * clock.minutes_in_hour * clock.seconds_in_minute);
-
-        // Incrementally rotate the light
         scene_light.transform.rotation *= Quaternion.Euler(Time.deltaTime * rotationX, 0f, 0f);
-
         clock.incrementSeconds(Time.deltaTime);
-        //Debug.Log(scene_light.transform.rotation + ", " + clock.hours);
+        CheckForNewHour();
     }
 
-    Vector3 FindLightPosition()
+    hour_position FindLightPosition()
     {
-        Vector3 result = Vector3.zero;
-        for(int i = 0; i < light_position.Count; i++)
+        hour_position result = new hour_position();
+        for (int i = 0; i < light_position.Count; i++)
         {
             if (light_position[i].hour == clock.hours)
             {
-                result = light_position[i].pos;
+                result = light_position[i];
                 break;
             }
 
         }
         return result;
     }
+    void SetCurrentHour(int new_hour)
+    {
+        current_hour = new_hour;
+    }
+    public void CheckForNewHour()
+    {
+        if (current_hour != clock.hours)
+        {
+            SetCurrentHour(clock.hours);
+            CheckForNewState();
+        }
+    }
+    void CheckForNewState()
+    {
+        int i = 0;
+        while(i < light_position.Count - 1)
+        {
+            if(current_hour >= light_position[i].hour && current_hour < light_position[i+1].hour)
+            {
+                current_day_state = light_position[i].state;
+                break;
+            }
+            i++;
+        }
+    }
+    
 }
 
 [System.Serializable]
@@ -54,6 +81,7 @@ public class hour_position
 {
     public int hour;
     public Vector3 pos;
+    public String state;
 }
 
 [System.Serializable]
@@ -114,30 +142,39 @@ public class Clock
     public void incrementSeconds(double val)
     {
         seconds += val;
-        if(seconds >= seconds_in_minute)
+
+        while (seconds >= seconds_in_minute && seconds - seconds_in_minute >= 0)
         {
+            seconds -= seconds_in_minute;
+
             minutes++;
-            if(minutes >= minutes_in_hour)
-            {
-                hours++;
-                if(hours >= hours_in_day)
-                {
-                    days++;
-                    if(days >= days_in_month)
-                    {
-                        months++;
-                        if(months >= months_in_year)
-                        {
-                            years++;
-                            months = 0;
-                        }
-                        days = 0;
-                    }
-                    hours = 0;
-                }
-                minutes = 0;
-            }
-            seconds = 0;
+        }
+        while(minutes >= minutes_in_hour && minutes - minutes_in_hour >= 0)
+        {
+            minutes -= minutes_in_hour;
+
+            hours++;
+        }
+
+        while(hours >= hours_in_day && hours - hours_in_day >= 0)
+        {
+            hours -= hours_in_day;
+
+            days++;
+        }
+
+        while(days >= days_in_month && days - days_in_month >= 0)
+        {
+            days -= days_in_month;
+
+            months++;
+        }
+
+        while (months >= months_in_year && months - months_in_year >= 0)
+        {
+            months -= months_in_year;
+
+            years++;
         }
     }
 }
--- Assets/Tests/Clock_Test.cs
+++ Assets/Tests/Clock_Test.cs
@@ -17,6 +17,66 @@ public class Clock_Test
         clock.incrementSeconds(59);
         Assert.AreEqual(clock.seconds, 0);
         Assert.AreEqual(clock.minutes, 1);
+        clock.incrementSeconds(3540);
+        Assert.AreEqual(clock.seconds, 0);
+        Assert.AreEqual(clock.hours, 1);
+        clock.incrementSeconds(82800);
+        Assert.AreEqual(clock.seconds, 0);
+        Assert.AreEqual(clock.hours, 0);
+        Assert.AreEqual(clock.days, 1);
+        // Use the Assert class to test conditions
+    }
+
+    [Test]
+    public void LightPosition_TestSimplePasses()
+    {
+        Clock clock = new Clock();
+        GameObject obj1 = new GameObject();
+        obj1.AddComponent<Day_Night_Cycle>();
+        Day_Night_Cycle cycle = obj1.GetComponent<Day_Night_Cycle>();
+        cycle.clock = clock;
+
+        List<hour_position> key_times = new List<hour_position>();
+        hour_position key_moment1 = new hour_position();
+        hour_position key_moment2 = new hour_position();
+        hour_position key_moment3 = new hour_position();
+        hour_position key_moment4 = new hour_position();
+
+        key_moment1.hour = 0;
+        key_moment1.pos = new Vector3(270, 0, 0);
+        key_moment1.state = "midnight";
+
+        key_moment2.hour = 3;
+        key_moment2.pos = new Vector3(315, 0, 0);
+        key_moment2.state = "night";
+
+        key_moment3.hour = 6;
+        key_moment3.pos = new Vector3(0, 0, 0);
+        key_moment3.state = "morning";
+
+        key_moment4.hour = 9;
+        key_moment4.pos = new Vector3(45, 0, 0);
+        key_moment4.state = "morning";
+
+        key_times.Add(key_moment1);
+        key_times.Add(key_moment2);
+        key_times.Add(key_moment3);
+        key_times.Add(key_moment4);
+
+        cycle.light_position = key_times;
+        cycle.CheckForNewHour();
+        Assert.AreEqual("midnight", cycle.current_day_state);
+        clock.incrementSeconds(3600 * 3);
+        cycle.CheckForNewHour();
+        Assert.AreEqual("night", cycle.current_day_state);
+        clock.incrementSeconds(3600 * 2);
+        cycle.CheckForNewHour();
+        Assert.AreEqual("night", cycle.current_day_state);
+
+        clock.incrementSeconds(3600);
+        cycle.CheckForNewHour();
+        Assert.AreEqual("morning", cycle.current_day_state);
+
         // Use the Assert class to test conditions
     }
 
